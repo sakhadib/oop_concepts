af4309a047293d3207db0819753cd701c00f8eba
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-08-16 16:19:31 +0600

    class

---
+++ inheritance_solution/src/course.java
@@ -0,0 +1,12 @@
+public class course {
+    public int id;
+    public String name;
+    public double credit;
+
+    // Constructor
+    public course(int id, String name, double credit) {
+        this.id = id;
+        this.name = name;
+        this.credit = credit;
+    }
+}
---
+++ inheritance_solution/src/student.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class student {
+    public int id;
+    public String name;
+    //public List<>
+}
