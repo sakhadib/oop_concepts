c5ad703802fd08468a0462398e3966365fe7b605
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-08-16 12:00:36 +0600

    java added

---
+++ general_input_output/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //for printing something directly
+        System.out.println("Printing this line directly");  //println = print line
+
+        //taking string and printing it directly
+        String s1 = "This is a static string";
+        System.out.println(s1);
+
+        //Appending a string with direct "" in println
+        String s2 = "This string is gonna be appended with  -";
+        System.out.println(s2+" this appending direct string");
+
+        //
+    }
+}
