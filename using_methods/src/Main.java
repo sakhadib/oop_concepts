34f959f16662aafe6488951a0b6dbf60ba7a177c
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-08-16 14:30:03 +0600

    using_methods done

---
+++ using_methods/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Calling add method to perform addition of two numbers
+        int a = 10;
+        int b = 20;
+        System.out.println(add(a, b));
+    }
+
+
+    // Creating Add method for adding two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
