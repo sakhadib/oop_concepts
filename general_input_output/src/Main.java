7d11a295051085d281a9ee3a5ecf3caebd55126e
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-08-16 12:24:35 +0600

    general I/O done

--- general_input_output/src/Main.java
+++ general_input_output/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //for printing something directly
@@ -11,6 +13,22 @@ public class Main {
         String s2 = "This string is gonna be appended with  -";
         System.out.println(s2+" this appending direct string");
 
-        //
+        //Appending 2 strings and adding them to create a third string
+        String s3 = "ami 1 number ";
+        String s4 = "ami 2 number";
+        String s5 = s3 + s4;
+        System.out.println(s5);
+
+        //Taking a string input and appending it with another to print a hello messege
+        Scanner scanner = new Scanner(System.in);   // there is no builtin console_readline for JAVA. we have to use scanner
+        System.out.println("what is your name...");
+        //String s6 = scanner.nextLine();
+        //System.out.println("Welcome onboard. You are, " + s6);
+
+        //Taking two integer and adding them.
+        int i1 = 10;
+        int i2 = 20;
+        int result_1 = i1 + i2;
+        System.out.println("Result is " + result_1);
     }
 }
